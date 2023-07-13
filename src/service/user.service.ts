894288c22975c302d51e11fdd6d8da23bbeb87e8
ShawnYuan <ysh83737@hotmail.com>
2023-07-13 09:40:25 +0800

    feat: 添加/getUserInfo接口

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,14 +1,20 @@
-import { Provide } from '@midwayjs/core';
-import { IUserOptions } from '../interface';
+import { Inject, Provide } from '@midwayjs/core';
+import { Context } from '@midwayjs/koa';
+import { InjectEntityModel } from '@midwayjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entity/user.entity';
 
 @Provide()
 export class UserService {
-  async getUser(options: IUserOptions) {
-    return {
-      uid: options.uid,
-      username: 'mockedName',
-      phone: '12345678901',
-      email: 'xxx.xxx@xxx.com',
-    };
+  @InjectEntityModel(User)
+  userEntity: Repository<User>;
+
+  @Inject()
+  ctx: Context;
+
+  async getUser() {
+    const { id } = this.ctx.getAttr('user.jwt');
+    const user = await this.userEntity.findOneBy({ id });
+    return user;
   }
 }
