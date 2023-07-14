c2a3cd825fc90e754ca3cb9fb596ef1cc160a754
ShawnYuan <ysh83737@hotmail.com>
2023-07-14 22:36:24 +0800

    feat: 添加列表类型的公共请求参数和返回数据结构

--- src/dto/common.dto.ts
+++ src/dto/common.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@midwayjs/swagger';
+import { Rule, RuleType } from '@midwayjs/validate';
 import { RESPONSE_CODE } from '../constants/responseCode';
 
 export class ResponseDTO<T = any> {
@@ -37,4 +38,41 @@ export class ResponseNullDTO extends ResponseDTO {
     example: null,
   })
   data: null;
+}
+
+/** 分页参数 */
+export class Pager {
+  @ApiProperty({
+    description: '页码',
+    type: 'integer',
+    minimum: 1,
+    example: 1,
+  })
+  @Rule(RuleType.number().integer().required().min(0))
+  page: number;
+
+  @ApiProperty({
+    description: '分页大小',
+    type: 'integer',
+    minimum: 1,
+    maximum: 200,
+    example: 10,
+  })
+  @Rule(RuleType.number().integer().required().min(0).max(200))
+  pageSize: number;
+}
+
+export class ListData<T = any> {
+  @ApiProperty({
+    description: '总数量',
+    example: 0,
+  })
+  total: number;
+
+  @ApiProperty({
+    description: '数据列表',
+    example: [],
+    type: 'array',
+  })
+  records: T[];
 }
