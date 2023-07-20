ad413a09bd40f3e4653d3535c850b79c7fe6454a
ShawnYuan <ysh83737@hotmail.com>
2023-07-20 10:10:58 +0800

    feat: 优化验证码接口的登录状态校验

--- src/middleware/basic/IgnoreLogin.ts
+++ src/middleware/basic/IgnoreLogin.ts
@@ -1,9 +1,11 @@
 import { Context } from 'koa';
+
+const PATHS = ['/login', '/captcha'];
 /**
- * 中间件忽略`/login`接口
+ * 中间件忽略登录相关无需验证的接口
  */
 export class IgnoreLogin {
   ignore(ctx: Context) {
-    return ctx.path === '/login';
+    return PATHS.includes(ctx.path);
   }
 }
