8cf3717ec42e3755bb8815780f37d5f1de637050
ShawnYuan <ysh83737@hotmail.com>
2023-07-25 20:58:57 +0800

    feat: 优化改写参数验证类错误的返回码

--- src/filter/validate.filter.ts
+++ src/filter/validate.filter.ts
@@ -1,5 +1,8 @@
 import { Catch, MidwayError } from '@midwayjs/core';
 import { MidwayValidationError } from '@midwayjs/validate';
+import { A_USER_ERROR } from '../constants/responseCode';
+
+const MIDWAY_VALIDATE_CODE = /^VALIDATE_/;
 
 /**
  * 处理参数校验类型的错误，使其返回系统的错误码（而非框架错误码）
@@ -11,6 +14,8 @@ export class ValidateErrorFilter {
     let code = err.code;
     if (cause instanceof MidwayError) {
       code = cause.code;
+    } else if (MIDWAY_VALIDATE_CODE.test(code as string)) {
+      code = A_USER_ERROR.REQ_PARAM_ERROR;
     }
     return {
       code,
