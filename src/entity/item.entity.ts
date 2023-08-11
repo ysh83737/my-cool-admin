c005616be23521901c795208b4c1b299176388bf
ShawnYuan <ysh83737@hotmail.com>
2023-08-11 10:00:03 +0800

    feat: 从商品数据表中定义商品列表数据的dto

--- src/entity/item.entity.ts
+++ src/entity/item.entity.ts
@@ -6,11 +6,17 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiProperty } from '@midwayjs/swagger';
 import { ITEM_STATUS } from '../interface/item.interface';
 
 /** 商品数据 */
 @Entity('item')
 export class Item {
+  @ApiProperty({
+    description: '商品id',
+    type: 'integer',
+    minimum: 1,
+  })
   @Index({ unique: true })
   @PrimaryGeneratedColumn({
     comment: '商品id',
@@ -29,18 +35,31 @@ export class Item {
   })
   updateTIme: Date;
 
+  @ApiProperty({
+    description: '商品名称',
+    maxLength: 100,
+  })
   @Column({
     comment: '商品名称',
     length: 100,
   })
   name: string;
 
+  @ApiProperty({
+    description: '商品描述',
+    maxLength: 500,
+  })
   @Column({
     comment: '商品描述，支持html',
     length: 500,
   })
   description: string;
 
+  @ApiProperty({
+    description: '商品状态 0-下架 1-正常',
+    type: 'enum',
+    enum: [ITEM_STATUS.AVAILABLE, ITEM_STATUS.DISABLED],
+  })
   @Column({
     comment: '商品状态 0-下架 1-正常',
     type: 'enum',
@@ -49,6 +68,11 @@ export class Item {
   })
   status: ITEM_STATUS;
 
+  @ApiProperty({
+    description: '库存数量',
+    type: 'integer',
+    minimum: 0,
+  })
   @Column({
     comment: '库存数量',
     type: 'integer',
@@ -56,6 +80,11 @@ export class Item {
   })
   stock: number;
 
+  @ApiProperty({
+    description: '已销数量',
+    type: 'integer',
+    minimum: 0,
+  })
   @Column({
     comment: '已销数量',
     type: 'integer',
